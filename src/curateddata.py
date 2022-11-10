5aa448eb52609108d7426aa424f4c8d7e9b7da33
sunilkn9 <111109003+sunilkn9@users.noreply.github.com>
2022-11-10 10:55:54 +0530

    Update curateddata.py

--- src/curateddata.py
+++ src/curateddata.py
@@ -14,7 +14,7 @@ class Started:
     def data_from_s3_curated(self):
         spark = SparkSession.builder.appName(" ").config('spark.ui.port', '4050').config(
             "spark.master", "local").enableHiveSupport().getOrCreate()
-        df = spark.read.csv("C:\\Users\\Sunil Kumar\\Downloads\\cleansed_log_details_data.csv", header=True, inferSchema=True)
+        df = spark.read.csv("C:\\Users\\Sunil Kumar\\Downloads\\cleansed_log_data_details.csv", header=True, inferSchema=True)
         df.show()
 
         """ aggregation"""
@@ -35,4 +35,56 @@ class Started:
                               .select("row_id", "day_hour","ip","no_put","no_post","no_head")
 
         log_per_device.show()
+
+
+       # log_per_device.write.mode("overwrite").saveAsTable("log_agg_per_device")
+       #log_per_device.write.csv("",header = True)
+        log_across_device = log_per_device.groupBy("day_hour") \
+                          .agg(count(col("ip")).alias("no_of_clients"), \
+                              sum(col('no_put')).alias("no_put"), \
+                               sum(col('no_post')).alias("no_post"), \
+                               sum(col('no_head')).alias("no_head"), \
+                              ).orderBy(asc("day_hour")).withColumn("row_id", monotonically_increasing_id())\
+                              .select("row_id", "day_hour","no_of_clients","no_put","no_post","no_head")
+
+        log_across_device.show()
+
+        sfOptions = {
+            "sfURL": r"",
+            "sfAccount": "",
+            "sfUser": "sunil",
+            "sfPassword": "Cloud@123",
+            "sfDatabase": "",
+            "sfSchema": "PUBLIC",
+            "sfWarehouse": "COMPUTE_WH",
+            "sfRole": "ACCOUNTADMIN"
+        }
+
+        curated.coalesce(1).write.format("snowflake").options(**sfOptions).option("dbtable",
+                                                                               "{}".format(
+                                                                                   r"curated_log_details")).mode(
+                "overwrite").options(header=True).save()
+        log_per_device.coalesce(1).write.format("snowflake").options(**sfOptions).option("dbtable",
+                                                                      "{}".format(
+                                                                          r"log_per_device")).mode(
+            "overwrite").options(header=True).save()
+
+        log_across_device.coalesce(1).write.format("snowflake").options(**sfOptions).option("dbtable",
+                                                                      "{}".format(
+                                                                          r"log_across_device")).mode(
+            "overwrite").options(header=True).save()
+
+if __name__ == '__main__':
+    try:
+        started = Started()
+    except Exception as e:
+        logging.error('Error at %s', 'Setup Object creation', exc_info=e)
+        sys.exit(1)
+
+    try:
+        started.data_from_s3_curated()
+    except Exception as e:
+        logging.error('Error at %s', 'read from s3 clean', exc_info=e)
+        sys.exit(1)
+
 
